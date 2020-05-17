bf8afd8734329a0f3ccc2a4f797f1e9132287d23
jayone <imdff0803@gmail.com>
2020-05-17 16:19:03 +0900

    update config ui, apply config ui value to main window - rewrite .yml file when config OK button clicked - remove unused variable in config.ui

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,10 +1,9 @@
 import os
 import sys
 import traceback
+import yaml
 
-from fbs_runtime.application_context.PyQt5 import (
-    ApplicationContext,
-)
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5 import QtGui, QtCore, QtWidgets
 
 # Custom
@@ -31,12 +30,11 @@ class ftpThread(QtCore.QThread):
 
 
 class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
-
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.ftp_cfg = get_cfg('config.yml')
         self.setupUi(self)
+        self.set_ftp_cfg("config.yml")
 
         # action
         self.actionConfig.triggered.connect(self.config)
@@ -47,23 +45,35 @@ class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         self.pushButtonDownload.setEnabled(False)
         self.pushButtonCancel.clicked.connect(self.cancel)
 
-        # set config
-        self.lineEditHost.setText(self.ftp_cfg['url'])
-        self.lineEditUser.setText(self.ftp_cfg['username'])
-        self.lineEditPassword.setText(self.ftp_cfg['password'])
-
         # list view setting
         self.to_download_model = QtGui.QStandardItemModel()
         self.downloaded_model = QtGui.QStandardItemModel()
         self.listViewFileToDownload.setModel(self.to_download_model)
         self.listViewDownloadComplete.setModel(self.downloaded_model)
 
-    def config(self):
-        filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', '.', '(*.yml)')[0]
+    def set_ftp_cfg(self, filepath: str) -> None:
+        self.ftp_cfg = ftp_cfg = get_cfg(filepath)
 
-        # TODO : 파일 내용 적용하기
+        # set config
+        self.lineEditHost.setText(ftp_cfg["url"])
+        self.lineEditUser.setText(ftp_cfg["username"])
+        self.lineEditPassword.setText(ftp_cfg["password"])
+
+    def config(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Open File", ".", "(*.yml)"
+        )[0]
+
+        if not filename:
+            return
+
         configDialog = ConfigDialog(self, filename)
-        configDialog.exec_()
+        result = configDialog.exec_()  # OK: 1, Cancel: 0
+
+        # OK
+        if result:
+            configDialog.make_yaml(filename)
+            self.set_ftp_cfg(filename)
 
     def apply(self):
         """다운로드 할 목록 리스트 뷰에 추가"""
@@ -83,7 +93,9 @@ class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
             ftp_client.set_active_mode()
         ftp_client.pattern = self.ftp_cfg["pattern"]
 
-        for remote_dir, local_dir in zip(self.ftp_cfg['remote_dirs'], self.ftp_cfg['local_dirs']):
+        for remote_dir, local_dir in zip(
+            self.ftp_cfg["remote_dirs"], self.ftp_cfg["local_dirs"]
+        ):
             self.ftp_client.apply_file_to_download(remote_dir, local_dir)
 
         for ftp_file in self.ftp_client.file_to_download:
@@ -96,11 +108,11 @@ class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         self.pushButtonApply.setEnabled(False)
         self.pushButtonDownload.setEnabled(True)
 
-        QtWidgets.QMessageBox.information(self, 'Done!', '파일 목록을 모두 가져왔습니다!')
+        QtWidgets.QMessageBox.information(self, "Done!", "Apply Complete!")
 
     def download(self):
         self.ftp_thread = ftpThread(self.ftp_client)
-        
+
         self.ftp_thread.download_complete_signal.connect(self.append_downloaded_file)
         self.ftp_thread.finished.connect(self.done)
         self.ftp_thread.start()
@@ -117,13 +129,11 @@ class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         self.progressBar.setValue(self.progressBar.value() + 1)
 
     def done(self):
-        self.pushButtonDownload.setEnabled(True)
-        QtWidgets.QMessageBox.information(self, 'Done!', 'Done download')
-        
+        QtWidgets.QMessageBox.information(self, "Done!", "Download Complete!")
 
     def ftp_download(self):
         self.ftp_thread = ftpThread()
-        
+
         self.ftp_thread.append_text_signal.connect(self.append_text)
         self.ftp_thread.finished.connect(self.done)
         self.ftp_thread.start()
@@ -132,7 +142,6 @@ class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
 
 
 class ConfigDialog(QtWidgets.QDialog, config.Ui_Dialog):
-
     def __init__(self, parent=None, filename=None):
         super(ConfigDialog, self).__init__(parent)
         self.setupUi(self)
@@ -141,19 +150,39 @@ class ConfigDialog(QtWidgets.QDialog, config.Ui_Dialog):
             return
 
         ftp_cfg = get_cfg(filename)
-        self.lineEditHost.setText(ftp_cfg['url'])
-        self.lineEditUsername.setText(ftp_cfg['username'])
-        self.lineEditPassword.setText(ftp_cfg['password'])
-        if ftp_cfg['passive_mode'] == False:
+        self.lineEditHost.setText(ftp_cfg["url"])
+        self.lineEditUsername.setText(ftp_cfg["username"])
+        self.lineEditPassword.setText(ftp_cfg["password"])
+        if ftp_cfg["passive_mode"] == False:
             self.radioButtonActive.setChecked(True)
         else:
             self.radioButtonDefault.setChecked(True)
-        self.textEditRemoteDirs.setText('\n'.join(ftp_cfg['remote_dirs']))
-        self.textEditLocalDirs.setText('\n'.join(ftp_cfg['local_dirs']))
-        self.lineEditFilePattern.setText(ftp_cfg['pattern'])
+        self.textEditRemoteDirs.setText("\n".join(ftp_cfg["remote_dirs"]))
+        self.textEditLocalDirs.setText("\n".join(ftp_cfg["local_dirs"]))
+        self.lineEditFilePattern.setText(ftp_cfg["pattern"])
+
+    def make_yaml(self, filepath: str):
+        ftp_cfg = dict()
+        ftp_cfg["url"] = self.lineEditHost.text()
+        ftp_cfg["username"] = self.lineEditUsername.text()
+        ftp_cfg["password"] = self.lineEditPassword.text()
+
+        is_passive_mode = False
+        if self.radioButtonDefault.isChecked() or self.radioButtonPassive.isChecked():
+            is_passive_mode = True
+        ftp_cfg["passive_mode"] = is_passive_mode
+
+        ftp_cfg["remote_dirs"] = self.textEditRemoteDirs.toPlainText().split("\n")
+        ftp_cfg["local_dirs"] = self.textEditLocalDirs.toPlainText().split("\n")
+        ftp_cfg["pattern"] = self.lineEditFilePattern.text()
+
+        config = dict(ftp=ftp_cfg)
+
+        with open(filepath, "w") as f:
+            yaml.dump(config, f)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     appctxt = ApplicationContext()
     window = MainWindow()
     window.show()
